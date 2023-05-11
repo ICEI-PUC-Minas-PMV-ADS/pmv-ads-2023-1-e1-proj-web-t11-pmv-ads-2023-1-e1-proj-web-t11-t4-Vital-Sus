36d4ff5d7e9edb9683abe208acfd815976cb6f91
PedroMendes <pedro.celso@sga.pucminas.br>
2023-05-10 21:08:30 -0300

    atualização

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,12 @@
 import React, { startTransition } from 'react';
 import TemplatePage from '../../template/template-page/templatePage';
-import "./Login.css";
+import "./Logintest.css";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import { Grid } from '@mui/material';
+import Grid from '@mui/material/Grid';
+
 
  
 const LoginPage = () => {
@@ -14,39 +15,44 @@ const LoginPage = () => {
    <div id="login">
         <h1 className="title">Entrar</h1>
         <form classname="form_login" action="./pages/homepage/homepage.css">
-           
-            <Grid xs={12} className="field">
-              
-              <TextField
-                helperText="Insira seu Email"
-                id="demo-helper-text-misaligned"
-                label="Email"
-                type='email'
-              />
             
-            </div>
             <div className="field">
-              <TextField
-               helperText="Insira a sua senha"
-               id="outlined-password-input"
-               label="Senha"
+                <TextField
+                  helperText="Insira seu Email"
+                  id="demo-helper-text-misaligned"
+                  label="Email"
+                  type='email'
+                />  
+            </div>
+        
+            <div className="field">
+                <TextField
+                helperText="Insira a sua senha"
+                id="outlined-password-input"
+                label="Senha"
                 type="password"
                 autoComplete="current-password"
-             />
-            
+               />
             </div>
-              <div className="actions">
-              <Button variant="contained">Entrar</Button>
-            
+
+            <div className="actions">
+                <Button variant="contained">Entrar</Button>
             </div>
         </form>  
-    </div> 
+    </div>
+    
     <div className='container'>
-       <img src="./public/img/Logo-white.png" alt="Logo_black"></img>
-       <h1>Seja bem vindo ao </h1>
+      <img src="/public/img/Logo-VitalSus_black" alt="Logo_black"></img>
+       <p className='titulo'> Seja bem vindo ao</p>
+       <div className='link'>
+        <p>Ainda não é cadastrado?
+            <a href="./pages/homePage/homePage"><b><u>Clique aqui</u></b></a>
+        </p>
+       </div>
     </div>
     </TemplatePage>
   );
 };
+
 
 export default LoginPage;
