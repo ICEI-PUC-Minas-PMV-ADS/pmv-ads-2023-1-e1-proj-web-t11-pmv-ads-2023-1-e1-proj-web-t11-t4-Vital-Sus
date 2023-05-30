d0d0448e9a4a03d38a27092fd5be62994ca2565d
PedroMendes <pedro.celso@sga.pucminas.br>
2023-05-30 19:30:52 -0300

    Atualização Doc - Plano de testes

--- src/utils/validacaoLogin.ts
+++ src/utils/validacaoLogin.ts
@@ -1,4 +1,4 @@
-import Alert from '@mui/material/Alert';
+import { Router } from "react-router-dom";
 
 export async function aprovarLogin(email: string, senha: string) {
   const validarEmail = (email: string): boolean => {
