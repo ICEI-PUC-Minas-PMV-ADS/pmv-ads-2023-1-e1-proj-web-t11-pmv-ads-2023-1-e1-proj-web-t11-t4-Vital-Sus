8048f428a370bfa3696bbe296e7842ab9d16b532
Julia Chaves <jchaves@ciandt.com>
2023-06-24 21:07:00 -0300

    Updates

---
+++ precache-manifest.216abc3235130ffd6550fae829b1cfa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a9282829e8a2dca731d",
+    "url": "/static/css/main.e3bf0ba7.chunk.css"
+  },
+  {
+    "revision": "5a9282829e8a2dca731d",
+    "url": "/static/js/main.8f8dedf4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b23fe9f2c4d4be926f08",
+    "url": "/static/js/2.c12c1d9e.chunk.js"
+  },
+  {
+    "revision": "b513439d713e32006632085f80ffdcd8",
+    "url": "/index.html"
+  }
+];
